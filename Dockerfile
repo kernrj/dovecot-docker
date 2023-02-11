ef372e037d25ff245cdca979537b2a816b383dc8
Rick Kern <kernrj@gmail.com>
2023-02-11 16:11:54 -0500

    Add docker files

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+FROM ubuntu:20.04
+
+ENV container=docker
+ENV LC_ALL C
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN echo "Etc/UTC" > /etc/timezone && \
+    rm -f /etc/localtime && \
+    ln -s /usr/share/zoneinfo/UTC /etc/localtime && \
+    apt-get -y update && \
+    apt-get upgrade -y && \
+    apt-get install -y \
+        curl \
+        gpg \
+        software-properties-common \
+        apt-transport-https \
+        ca-certificates \
+        ssl-cert \
+        tini
+
+RUN curl -L https://repo.dovecot.org/DOVECOT-REPO-GPG | gpg --import && \
+    gpg --export ED409DA1 > /etc/apt/trusted.gpg.d/dovecot.gpg && \
+    add-apt-repository "deb https://repo.dovecot.org/ce-2.3-latest/ubuntu/focal focal main" && \
+    apt-get -y install \
+        dovecot-core \
+        dovecot-gssapi \
+        dovecot-imapd \
+        dovecot-ldap \
+        dovecot-lmtpd \
+        dovecot-lua \
+        dovecot-lucene \
+        dovecot-managesieved \
+        dovecot-mysql \
+        dovecot-pgsql \
+        dovecot-pop3d \
+        dovecot-sieve \
+        dovecot-solr \
+        dovecot-sqlite \
+        dovecot-submissiond && \
+    groupadd -g 1000 vmail && \
+    useradd -u 1000 -g vmail vmail -d /var/spool/vmail && \
+    passwd -l vmail && \
+    rm -rf /etc/dovecot && \
+    mkdir -p /var/spool/vmail /etc/dovecot && \
+    chown vmail:vmail /var/spool/vmail
+ 
+COPY start-dovecot.sh /bin/start-dovecot.sh
+COPY dovecot.conf /etc/dovecot/dovecot.conf
+
+RUN chmod 755 /bin/start-dovecot.sh
+
+VOLUME ["/etc/dovecot", "/var/spool/vmail"]
+
+ENTRYPOINT ["/usr/bin/tini", "--"]
+CMD ["/bin/start-dovecot.sh"]
---
+++ start-dovecot.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -e
+set -o pipefail
+
+function handleSig {
+    echo "Stopping dovecot..."
+    dovecot stop
+    echo "Dovecot stopped."
+}
+
+trap handleSig SIGINT SIGTERM SIGQUIT SIGHUP
+
+if [ "$POSTMASTER_EMAIL" == "" ]; then
+    echo "Specify the postmaster email WITH POSTMASTER_EMAIL" >&2
+    exit 1;
+fi
+
+readonly CERT_FILE=/etc/dovecot/cert.pem
+readonly KEY_FILE=/etc/dovecot/privkey.pem
+
+FILE_RETRIES=0
+readonly FILE_MAX_RETRIES=20
+while [ ! -f "$CERT_FILE" ] && [ "$FILE_RETRIES" -lt "$FILE_MAX_RETRIES" ]; do
+    echo "Certificate file [$CERT_FILE] does not exist. Waiting..."
+    ls -l $(dirname "$CERT_FILE")
+    sleep 5
+    ((FILE_RETRIES++)) || true
+done
+
+if [ ! -f "$CERT_FILE" ]; then
+    echo "Certificate file [$CERT_FILE] not found. Exiting." >&2
+    exit 1
+fi
+
+FILE_RETRIES=0
+readonly
+while [ ! -f "$KEY_FILE" ] && [ "$FILE_RETRIES" -lt "$FILE_MAX_RETRIES" ]; do
+    echo "Private key file [$KEY_FILE] does not exist. Waiting..."
+    sleep 5
+    ((FILE_RETRIES++)) || true
+done
+
+if [ ! -f "$KEY_FILE" ]; then
+    echo "Private key file [$KEY_FILE] not found. Exiting." >&2
+    exit 1
+fi
+
+echo "Starting dovecot"
+dovecot -F &
+
+cat /etc/dovecot/dovecot.conf | \
+    sed -E "s/postmaster_address[= ].*\$/postmaster_address = ${POSTMASTER_EMAIL}/" \
+	> /tmp/dovecot.conf
+
+mv /tmp/dovecot.conf /etc/dovecot/dovecot.conf
+
+cat /etc/dovecot/dovecot.conf
+
+echo "Dovecot started."
+
+CERT_MD5=$(md5sum "$CERT_FILE" | cut -d ' ' -f1)
+KEY_MD5=$(md5sum "$KEY_FILE" | cut -d ' ' -f1)
+
+while true; do
+    NEW_CERT_MD5=$(md5sum "$CERT_FILE" | cut -d ' ' -f1)
+    NEW_KEY_MD5=$(md5sum "$KEY_FILE" | cut -d ' ' -f1)
+
+    if [ "$NEW_CERT_MD5" != "$CERT_MD5" ] || [ "$NEW_KEY_MD5" != "$KEY_MD5" ]; then
+	CERT_MD5="$NEW_CERT_MD5"
+	KEY_MD5="$NEW_KEY_MD5"
+
+	echo "Certificate or private key changed. Reloading."
+	dovecot reload
+    fi
+
+    sleep 60 &
+    wait $!
+done
